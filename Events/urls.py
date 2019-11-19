a1226ad414567ee95496893507a089c70d559f85
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2019-11-19 09:06:13 -0500

    Created some dummy data, have a minimal list and detail view working

--- Events/urls.py
+++ Events/urls.py
@@ -7,10 +7,11 @@ from . import views   # '.' = current directory
 
 urlpatterns = [
   path('', 
-    views.home, name='events-home'),
+    # views.home, name='events-home'),
+    EventListView.as_view(), name='events-home'),
 
-  path('test/', 
-    EventListView.as_view(), name='event-list'),
+  # path('test/', 
+  #   EventListView.as_view(), name='event-list'),
   
   path('event/<int:pk>/',
     EventDetailView.as_view(), name='event-detail'),
