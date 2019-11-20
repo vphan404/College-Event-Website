695369ce3aec68b86952512655287ee3ce5a66bd
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2019-11-20 07:01:21 -0500

    Made a user login and logout page

--- college_event_website/urls.py
+++ college_event_website/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('register/', include('Users.urls')),
     path('rso/', include('Rso.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
 ]
