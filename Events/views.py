b82ccc485e85a96775770cdb64a7d80ef9bc4bdf
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2019-11-19 23:33:33 -0500

    Removed dummy data

--- Events/views.py
+++ Events/views.py
@@ -18,35 +18,6 @@ from .forms import (
 
 # Create your views here.
 
-# Temporary dummy variable representing what a SQL query could return
-dummyPosts = [
-  {
-    'author': 'Bobby Jackson',
-    'title': 'Blog Post 1',
-    'content': 'First post content goes here.',
-    'date_posted': 'October 30, 2019'
-  },
-  {
-    'author': 'James Sullivan III',
-    'title': 'Blog Post 2',
-    'content': "Second post content goes here. It's a pretty spoopy day today",
-    'date_posted': 'October 31, 2019'
-  },
-]
-
-
-# dummyEvents = [
-#   {
-#     'eventName': 'My First Event',
-#     'eventDescription': 'Some description goes here, can be arbitrarily long',
-#     'eventDatetime': 'October 30, 2019'
-#   }, 
-#   {
-#     'eventName': 'Second Event',
-#     'eventDescription': 'Some other description goes here.',
-#     'eventDatetime': 'October 31, 2019'
-#   }
-# ]
 
 # Homepage
 def home(request):
