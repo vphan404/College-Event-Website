e79b9e27a4f2d98029f120cd6259b02a1e1db9e8
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2019-11-20 07:27:21 -0500

    Created a very simple profile page, and added it to db

--- Users/models.py
+++ Users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from PIL import Image
 
 # Create your models here.
 def defaultUser():
@@ -14,4 +15,19 @@ class User(AbstractUser):
   is_admin = models.BooleanField(default=False) 
   is_super_admin = models.BooleanField(default=False) 
 
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+  def __str__(self):
+    return f'{self.user.username} Profile'
+
+  def save(self, **kwargs):
+    super().save()
+
+    image = Image.open(self.image.path) 
+
+    if image.width > 300 or image.height > 300:
+      output_size = (300, 300) 
+      image.thumbnail(output_size)
+      image.save(self.image.path)
